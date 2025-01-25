d91fd579261ed1d69bd3735d09a838a96c0558b0
reniby <52298111+reniby@users.noreply.github.com>
2025-01-25 18:22:53 -0500

    Obstacle Types

--- Scripts/obstacle.gd
+++ Scripts/obstacle.gd
@@ -1,8 +1,11 @@
 extends Node3D
 
 @onready var mesh: Node3D = $Area3D/Mesh
+@export var obstacle_type : int
+
+var speed_changes = [1.0, 5.0]
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.is_in_group("Player"):
-		body.speed = 1.0
+		body.speed = speed_changes[obstacle_type]
 		mesh.visible = false
