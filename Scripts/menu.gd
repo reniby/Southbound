c592bfac5827375034a22691c629a1d178e4ff35
reniby <52298111+reniby@users.noreply.github.com>
2025-01-26 19:05:11 -0500

    Audio and UI

---
+++ Scripts/menu.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/World.tscn")
+
+func _on_zen_pressed() -> void:
+	pass # Replace with function body.
