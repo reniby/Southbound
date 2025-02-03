cc945c5c8c260e368b68fe0bb7483e3b771a7bdb
reniby <52298111+reniby@users.noreply.github.com>
2025-02-02 19:57:07 -0500

    souuuuund

--- Scripts/song.gd
+++ Scripts/song.gd
@@ -1,7 +1,6 @@
 extends Node3D
+
 @onready var song: AudioStreamPlayer = $AudioStreamPlayer
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	song.play()
